bb5eaaa3c4b14cc9a4262d96c2e84d269a29411e
ThiagoORuby <trs@ic.ufal.br>
2024-01-09 20:06:28 -0300

    Cria usuário padrão

--- backend/database.py
+++ backend/database.py
@@ -1,3 +1,5 @@
+from models import Document, User
+from passlib import hash
 from settings import settings
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -9,6 +11,41 @@ engine = create_engine(settings.DATABASE_URL,
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def create_default_user():
+
+    password = hash.bcrypt.hash("admin")
+    email = "admin@gmail.com"
+
+    user_data = {"username": "admin", "password": password, "email": email}
+
+    _user = User(**user_data)
+
+    session = SessionLocal()
+
+    if session.query(User).filter_by(email=email).first():
+        return
+
+    session.add(_user)
+    session.commit()
+    session.refresh(_user)
+
+    docs_data = [{
+        "title": "Welcome",
+        "body": "This is the default document",
+    }, {
+        "title": "Hello",
+        "body": "This is the default document",
+    }]
+
+    for doc in docs_data:
+
+        _doc = Document(**doc, user_id=_user.id)
+
+        session.add(_doc)
+        session.commit()
+        session.refresh(_doc)
 
 
 def get_session():
