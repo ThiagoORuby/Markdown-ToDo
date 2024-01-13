9357f25b02cf229bf93e02d14be947995fd03b4f
ThiagoORuby <trs@ic.ufal.br>
2024-01-13 18:58:03 -0300

    Alterações simples

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+    reactStrictMode: false,
+    rewrites: () => {
+      return [
+        {
+          source: "/api/:path*",
+          destination: "http://localhost:8000/api/:path*",
+        },
+      ]
+    },
+  }
 
 module.exports = nextConfig
